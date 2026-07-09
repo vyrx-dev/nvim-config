2d40b7e53b74106985d495e3684291edd1856bb4
Amit <theamit.969@gmail.com>
2026-07-10 02:30:54 +0530

    kanagawa with few tweaks for golang

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,16 @@
 return {
   { "ellisonleao/gruvbox.nvim", lazy = true, opts = { transparent_mode = false } },
   { "folke/tokyonight.nvim", lazy = true, opts = { style = "night", transparent = false } },
-  { "rebelot/kanagawa.nvim", lazy = true, opts = {} },
   { "rose-pine/neovim", lazy = true, opts = {} },
   { "metalelf0/black-metal-theme-neovim", lazy = true, opts = {} },
-  { "vague2k/vague.nvim", priority = 1000, lazy = true, opts = { transparent = false } }, -- fav theme
-
-  -- only this will be loaded on startup
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = { transparent = false },
-    config = function(_, opts)
-      require("solarized-osaka").setup(opts)
-      vim.cmd.colorscheme "solarized-osaka"
-    end,
-  },
+  { "craftzdog/solarized-osaka.nvim", lazy = true, opts = { transparent = false } },
+  { "vague-theme/vague.nvim", lazy = true, opts = { transparent = false } },
 
   -- superior gruvbox variant
   {
     "sainnhe/gruvbox-material",
     lazy = true,
+    priority = 1000,
     config = function()
       vim.g.gruvbox_material_enable_italic = true
       vim.g.gruvbox_material_background = "hard"
@@ -29,6 +18,7 @@ return {
     end,
   },
 
+  -- leader + tt to toggle transparency
   {
     "xiyaowong/transparent.nvim",
     lazy = false,
@@ -41,5 +31,40 @@ return {
         "NvimTreeWinSeparator",
       },
     },
+  },
+
+  -- only this theme will be loaded on startup
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      keywordStyle = {
+        italic = false,
+      },
+
+      -- Remove gutter background
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          ["@keyword.coroutine"] = { fg = theme.syn.special, bold = true },
+          ["@keyword.function"] = { fg = theme.syn.fun, bold = true },
+        }
+      end,
+    },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+      vim.cmd "colorscheme kanagawa"
+    end,
   },
 }
